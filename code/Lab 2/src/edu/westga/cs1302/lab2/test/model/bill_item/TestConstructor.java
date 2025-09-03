6d92eeca49d202948b702ec2c026ca1d9854235f
Valor Bright <lavendermothwings>
2025-09-03 16:05:53 -0400

    Added JUnit tests 1) Added a test for billItem Constructor 2) Added a test for getText

--- code/Lab 2/src/edu/westga/cs1302/lab2/test/model/bill_item/TestConstructor.java
+++ code/Lab 2/src/edu/westga/cs1302/lab2/test/model/bill_item/TestConstructor.java
@@ -4,11 +4,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
+
 class TestConstructor {
 
+	//test: billitem null name, billitem 0 or neg, billitem name, billitem amount
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testBillItemName() {
+		BillItem apple = new BillItem("apple", 3.0);
+		assertEquals(apple.getName(), "apple");		
+	}
+	
+	
+	@Test
+	void testBillItemAmount() {
+		BillItem apple = new BillItem("apple", 3.0);
+		assertEquals(apple.getAmount(), 3.0);		
+	}
+	
+	@Test
+	void testBillItemNameIsNull() {
+		assertThrows(IllegalArgumentException.class,
+				() -> new BillItem(null, 3.0));
 	}
 
+	@Test
+	void testBillItemAmountIsZero() {
+		assertThrows(IllegalArgumentException.class,
+				() -> new BillItem("apple", 0));
+	}
+	
+	@Test
+	void testBillItemAmountIsNegative() {
+		assertThrows(IllegalArgumentException.class,
+				() -> new BillItem("apple", -1));
+	}
 }
--- code/Lab 2/src/edu/westga/cs1302/lab2/test/view/bill_view/TestGetText.java
+++ code/Lab 2/src/edu/westga/cs1302/lab2/test/view/bill_view/TestGetText.java
@@ -4,11 +4,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
+import edu.westga.cs1302.lab2.view.BillView;
+
+
 class TestGetText {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void testViewBillWithOneItem() {
+		Bill myBill = new Bill();
+		BillItem apple = new BillItem("apple", 3.0);
+		BillView billView = new BillView();
+		String bill; 
+		
+		myBill.addItem(apple);
+		bill = billView.getText(myBill);
+		
+		assertEquals(bill.substring(0, 5), "ITEMS");
+		assertEquals(bill.substring(7, 12), "apple");
+		assertEquals(bill.substring(25, 37), "TOTAL - $3.0");
+		
+		System.out.print(bill);
 
+	}
+	
+	@Test
+	public void testViewBillWithTwoItems() {
+		Bill myBill = new Bill();
+		BillItem apple = new BillItem("apple", 3.0);
+		BillItem pear = new BillItem("pear", 4.0);
+		BillView billView = new BillView();
+		String bill; 
+		
+		myBill.addItem(apple);
+		myBill.addItem(pear);
+		bill = billView.getText(myBill);
+		System.out.print(bill);
+		
+		assertEquals(bill.substring(0, 5), "ITEMS");
+		assertEquals(bill.substring(7, 12), "apple");
+		assertEquals(bill.substring(20, 24), "pear");
+		assertEquals(bill.substring(105, 117), "TOTAL - $9.1");
+		
+		
+	
+
+	}
 }
+
+
